2e4bd2b67e27e057843993bbcbce609f1e33fcfd
Huu-Huan Nguyen <nguyenhuands99@gmail.com>
2018-08-23 17:13:27 +0700

    Create parameters for main()

--- fibo/Main.cpp
+++ fibo/Main.cpp
@@ -1,18 +1,38 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define MY_FILE "D:\\OneDrive - VNU-HCMUS\\Huan\\Programming Projects\\Visual Studio Projects\\[DSAA]Fibonacci\\fibo-sequence\\fibo\\Debug\\fibo.exe"
 #include "FiboHeader.h"
 #include "FiboIO.h"
 #include "PowerHeader.h"
 #include <iostream>
 #include <stdlib.h>
+#include <sstream>
 
 using namespace std;
 
-int main()
+int main(int argc, const char *argv[])
 {
+	argc = 3;
+	argv[0] = MY_FILE; 
+	argv[1] = "2";
+	argv[2] = "50";
+	if (argc != 3)
+		return 0;
+	int alg = atoi(argv[1]);
+	unsigned long long(*fib)(unsigned long);
+	switch (alg)
+	{
+	case 1: fib = &fibo1; break;
+	case 2: fib = &fibo2; break;
+	case 3: fib = &fibo3; break;
+	case 4: fib = &fibo4; break;
+	default: return 0;
+	}
 	unsigned long n;
-	fiboInput(n);
-	fiboOutput(n);
-	//powerOutput(n);
+
+	stringstream ss(argv[2]);
+	ss >> n;
+
+	cout << "F[" << n << "] = " << fib(n);
 
 	printf("\n");
 	system("pause");
